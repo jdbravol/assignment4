6799daf56d545ed7f9e01a62180fce130276b1a0
santiagoecheverri94 <santiagoecheverri94@gmail.com>
2016-10-09 20:35:25 -0500

    Add files via upload
    Changed some headers, added two classes

---
+++ CritterWorld.java
@@ -0,0 +1,20 @@
+/* CRITTERS CritterWorld.java
+ * EE422C Project 4 submission by
+ * Juan Bravo
+ * jdb5338
+ * 16475
+ * Santiago Echeverri
+ * se7365
+ * 16470
+ * Slip days used: <0>
+ * Fall 2016
+ */
+
+package assignment4;
+
+import java.util.*;
+
+public class CritterWorld {
+	private static HashSet<Critter> livingCritters;			// this hash set keeps track of all the critters alive at one time
+	private static CritterLocation[][] locationMatrix;		// this matrix keeps track of critters at each specific location
+}
